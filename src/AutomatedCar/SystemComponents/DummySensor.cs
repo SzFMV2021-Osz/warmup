97d90851ddd1fe2fe36faf5b5d6fd88b0d66e71e
pintergreg <pintergreg@gmail.com>
2021-09-16 11:01:29 +0200

    Add empty DummySensor

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,12 @@
+namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+
+    /// <summary>This is a dummy sensor for demonstrating the codebase.
+    /// It calculates distance per coordinate between the controlled car and the dummy Circle object</summary>
+    public class DummySensor : SystemComponent
+    {
+
+    }
+}
