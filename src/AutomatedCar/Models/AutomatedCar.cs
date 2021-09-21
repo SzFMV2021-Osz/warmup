4f9caef633ef1ec5bf7e3264aca3ae831a5b6a7d
torokt21 <torokt21@gmail.com>
2021-09-21 12:15:36 +0200

    Implement dummy sensor #1

--- src/AutomatedCar/Models/AutomatedCar.cs
+++ src/AutomatedCar/Models/AutomatedCar.cs
@@ -10,7 +10,11 @@ namespace AutomatedCar.Models
         public AutomatedCar(int x, int y, string filename)
             : base(x, y, filename)
         {
+            
+
             this.virtualFunctionBus = new VirtualFunctionBus();
+            this.dummySensor = new DummySensor(this.virtualFunctionBus);
+            this.virtualFunctionBus.RegisterComponent(this.dummySensor);
             this.ZIndex = 10;
         }
 
@@ -20,13 +24,15 @@ namespace AutomatedCar.Models
         public int Velocity { get; set; }
         public Geometry Geometry { get; set; }
 
-        /// <summary>Starts the automated cor by starting the ticker in the Virtual Function Bus, that cyclically calls the system components.</summary>
+        private DummySensor dummySensor { get; }
+
+        /// <summary>Starts the automated car by starting the ticker in the Virtual Function Bus, that cyclically calls the system components.</summary>
         public void Start()
         {
             this.virtualFunctionBus.Start();
         }
 
-        /// <summary>Stops the automated cor by stopping the ticker in the Virtual Function Bus, that cyclically calls the system components.</summary>
+        /// <summary>Stops the automated car by stopping the ticker in the Virtual Function Bus, that cyclically calls the system components.</summary>
         public void Stop()
         {
             this.virtualFunctionBus.Stop();
---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,31 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DummySensor : SystemComponent
+    {
+        DummyPacket dummyPacket { get; } = new DummyPacket();
+
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+            this.virtualFunctionBus.DummyPacket = this.dummyPacket;
+        }
+
+        public override void Process()
+        {
+            AutomatedCar automatedCar = World.Instance.ControlledCar;
+            Circle circle = World.Instance.WorldObjects
+                .OfType<Circle>()
+                .First();
+
+            this.dummyPacket.DistanceX = Math.Abs(automatedCar.X - circle.X);
+            this.dummyPacket.DistanceY = Math.Abs(automatedCar.Y - circle.Y);
+        }
+    }
+}
