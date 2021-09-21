2a405216ba68cb38615b1163471c77d95fecaa57
Bertalan Bernát <bernat.bertalan99@gmail.com>
2021-09-21 21:02:20 +0200

    DummySensor implemented

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,29 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using Models;
+    using Packets;
+    using System;
+
+    public class DummySensor : SystemComponent
+    {
+        private DummyPacket dummyPacket;
+
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+            this.dummyPacket = new DummyPacket();
+            virtualFunctionBus.DummyPacket = dummyPacket;
+        }
+
+        /// <summary>
+        /// Process method to calculate the distance x and y.
+        /// </summary>
+        public override void Process()
+        {
+            AutomatedCar car = World.Instance.ControlledCar;
+            Circle circle = (Circle)World.Instance.WorldObjects[0];
+
+            this.dummyPacket.DistanceX = Math.Abs(circle.X - car.X);
+            this.dummyPacket.DistanceY = Math.Abs(circle.Y - car.Y);
+        }
+    }
+}
