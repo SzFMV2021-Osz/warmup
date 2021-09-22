c214f3baee9739b1664e0a5d80abd7d23a93e18a
BCXFMD <adriandrotartech@gmail.com>
2021-09-22 23:17:58 +0200

    Dummy Sensor implemented #1

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AutomatedCar.Models;
+using AutomatedCar.SystemComponents.Packets;
+
+namespace AutomatedCar.SystemComponents
+{
+    /// <summary>
+    /// Sensor for calculating the egocar - circle distance.
+    /// </summary>
+    public class DummySensor : SystemComponent
+    {
+        private DummyPacket dummyPacket;
+
+        /// <summary>
+        /// Initializes new instance of <see cref="DummySensor"/>.
+        /// </summary>
+        /// <param name="virtualFunctionBus">dummyPacket reference.</param>
+        public DummySensor(VirtualFunctionBus virtualFunctionBus)
+            : base(virtualFunctionBus)
+        {
+            this.dummyPacket = new DummyPacket();
+            virtualFunctionBus.DummyPacket = this.dummyPacket;
+        }
+
+        /// <summary>
+        /// Egocar - circle distance calculation.
+        /// </summary>
+        public override void Process()
+        {
+            AutomatedCar car = World.Instance.ControlledCar;
+            Circle circle = (Circle)World.Instance.WorldObjects[0];
+
+            this.dummyPacket.DistanceX = Math.Abs(car.X - circle.X);
+            this.dummyPacket.DistanceY = Math.Abs(car.Y - circle.Y);
+        }
+    }
+}
