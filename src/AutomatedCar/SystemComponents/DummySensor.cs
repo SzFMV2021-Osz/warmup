21069d6260a1259830795f23159feab1951ec4f3
Horvath, Levente <levente.horvath@ge.com>
2021-09-22 13:58:31 +0200

    DummySensor implementation

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,33 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+
+    public class DummySensor : SystemComponent
+    {
+        private DummyPacket dummyPacket;
+
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+            this.dummyPacket = new DummyPacket();
+            virtualFunctionBus.DummyPacket = dummyPacket;
+        }
+
+        public override void Process()
+        {
+            Circle circleObject = (Circle)World.Instance.WorldObjects.ElementAt(0);
+            Car carObject = World.Instance.ControlledCar;
+
+            dummyPacket.DistanceX = Math.Abs(circleObject.X - carObject.X);
+            dummyPacket.DistanceY = Math.Abs(circleObject.Y - carObject.Y);
+
+            virtualFunctionBus.DummyPacket = dummyPacket;
+        }
+    }
+}
